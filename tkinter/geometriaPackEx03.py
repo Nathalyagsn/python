9070d2e9f078bcb75747f552ce17323013e7bb42
Nathalya Silva <nathalya.gsn@gmail.com>
2025-03-21 11:44:00 -0300

    Geometria Pack Ex04

---
+++ tkinter/geometriaPackEx03.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+janela = tk.Tk()
+janela.title("Exemplo de Pack")
+
+
+lbl1 = ttk.Label(janela, text="Label 1", width=10)
+lbl2 = ttk.Label(janela, text="Label 2", width=10)
+lbl3 = ttk.Label(janela, text="Label 3", width=10)
+lbl4 = ttk.Label(janela, text="Label 4", width=10)
