d8074c44195c4f553c96017a3b3efaeb6d3b3e37
Nathalya Silva <nathalya.gsn@gmail.com>
2025-03-21 11:35:12 -0300

    Geometria com Pack Ex 02

--- tkinter/geometriaPackEx02.py
+++ tkinter/geometriaPackEx02.py
@@ -3,3 +3,16 @@ from tkinter import ttk
 
 janela = tk.Tk()
 janela.title("Exemplo de Pack")
+
+btn1 = ttk.Button(janela, text="Top", width=10)
+btn2 = ttk.Button(janela, text="Bottom", width=10)
+btn3 = ttk.Button(janela, text="Left", width=10)
+btn4 = ttk.Button(janela, text="Right", width=10)
+
+btn1.pack(side="top", pady=5)
+btn2.pack(side="bottom", pady=5)
+btn3.pack(side="left", padx=15)
+btn4.pack(side="right", padx=5)
+
+
+janela.mainloop()
